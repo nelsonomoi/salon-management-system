c9f5bfa728db3799edb7c9c0c9adc91c75bb1a4e
nelsonomoi <omoinelson3@gmail.com>
2019-06-21 18:13:58 +0300

    change nav-header

--- resources/views/book.blade.php
+++ resources/views/book.blade.php
@@ -3,11 +3,42 @@
 
 @section('content')
     <div class="container">
-        <div class="card">
-            <div class="card-header text-center">
-                Book Now !
-            </div>
-
-        </div>
+        {{--<div class="card">--}}
+            {{--<div class="card-header text-center">--}}
+                {{--Book Now !--}}
+            {{--</div>--}}
+                {{--<div class="row">--}}
+                    {{--<div class="col-2 ">--}}
+                       {{--<strong>Time</strong>--}}
+                    {{--</div>--}}
+                    {{--<div class="col-6">--}}
+                        {{--<strong>Name of service provider</strong>--}}
+                    {{--</div>--}}
+                    {{--<div class="col-sm">--}}
+                        {{--<strong>Slots availlable</strong>--}}
+                    {{--</div>--}}
+                    {{--<div class="col-sm">--}}
+                        {{--<strong>Status</strong>--}}
+                    {{--</div>--}}
+                {{--</div>--}}
+            {{--<ul class="list-group">--}}
+                {{--<li class="list-group-item">--}}
+                    {{--<div class="row">--}}
+                        {{--<div class="col-2">--}}
+                            {{--Time--}}
+                        {{--</div>--}}
+                        {{--<div class="col-6">--}}
+                            {{--Name of Service Provider--}}
+                        {{--</div>--}}
+                        {{--<div class="col-sm">--}}
+                            {{--Slots availlable--}}
+                        {{--</div>--}}
+                        {{--<div class="col-sm">--}}
+                            {{--Status--}}
+                        {{--</div>--}}
+                    {{--</div>--}}
+                {{--</li>--}}
+            {{--</ul>--}}
+        {{--</div>--}}
     </div>
 @endsection
